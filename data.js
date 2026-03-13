e4dc378c4185460ce48a07916d1d20595e72132d
Jerzy Zasadni <jerzy.zasadni@gmail.com>
2026-03-13 11:44:47 +0100

    Update data.js

--- data.js
+++ data.js
@@ -48,8 +48,8 @@ var APP_DATA = {
       ],
       "infoHotspots": [
         {
-          "yaw": -1.2,
-          "pitch": 0.1000,
+          "yaw": -1.3,
+          "pitch": -0.1000,
           "title": "Kramnica Rock",
           "text": "Text"
         },
